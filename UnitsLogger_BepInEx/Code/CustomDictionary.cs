662a2183830b2de1832b8188a5518401ab1736ee
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-06-12 01:55:19 +0300

    Optimized CustomDictionary

--- UnitsLogger_BepInEx/Code/CustomDictionary.cs
+++ UnitsLogger_BepInEx/Code/CustomDictionary.cs
@@ -2,36 +2,33 @@
 using System.Collections.Generic;
 using System.Text;
 using UnitsLogger_BepInEx.Resources;
+using System.Collections.Concurrent;
 
 namespace UnitsLogger_BepInEx
 {
     public static class CustomDictionary
     {
-        public static Dictionary<string, string> ru = JsonConvert.DeserializeObject<Dictionary<string, string>>(Encoding.UTF8.GetString(Resource.ru).Trim('\uFEFF'));
-        public static Dictionary<string, string> en = JsonConvert.DeserializeObject<Dictionary<string, string>>(Encoding.UTF8.GetString(Resource.en).Trim('\uFEFF'));
+        private static readonly ConcurrentDictionary<string, string> ru;
+        private static readonly ConcurrentDictionary<string, string> en;
+
+        static CustomDictionary()
+        {
+            ru = JsonConvert.DeserializeObject<ConcurrentDictionary<string, string>>(Encoding.UTF8.GetString(Resource.ru).Trim('\uFEFF'));
+            en = JsonConvert.DeserializeObject<ConcurrentDictionary<string, string>>(Encoding.UTF8.GetString(Resource.en).Trim('\uFEFF'));
+        }
 
         public static string GetLocal(this string key)
         {
-            if ((string)Reflection.GetField(LocalizedTextManager.instance.GetType(), LocalizedTextManager.instance, "language") == "ru")
-            {
-                return ru.ContainsKey(key) ? ru[key] : key;
-            }
-            else
-            {
-                return en.ContainsKey(key) ? en[key] : key;
-            }
+            var lang = (string)Reflection.GetField(LocalizedTextManager.instance.GetType(), LocalizedTextManager.instance, "language");
+            var dictionary = lang == "ru" ? ru : en;
+            return dictionary.TryGetValue(key, out var result) ? result : key;
         }
 
         public static void SetLocal(this string key, string value)
         {
-            if ((string)Reflection.GetField(LocalizedTextManager.instance.GetType(), LocalizedTextManager.instance, "language") == "ru")
-            {
-                ru[key] = value;
-            }
-            else
-            {
-                en[key] = value;
-            }
+            var lang = (string)Reflection.GetField(LocalizedTextManager.instance.GetType(), LocalizedTextManager.instance, "language");
+            var dictionary = lang == "ru" ? ru : en;
+            dictionary[key] = value;
         }
     }
 }
